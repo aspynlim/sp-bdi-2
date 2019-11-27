da17be8db67a2009a1eef56a818e6980eed148d0
Jen Lim <cmdlhz@gmail.com>
2019-11-27 15:23:36 +0900

    2019-11-27 UserVO

---
+++ src/main/java/com/sp/bdi2/user/UserVO.java
@@ -0,0 +1,16 @@
+package com.sp.bdi2.user;
+
+import lombok.Data;
+
+@Data
+public class UserVO {
+	private String uiName;
+	private String active;
+	private String uiId;
+	private Integer uiNum;
+	private String uiPwd;
+	private String credat;
+	private String cretim;
+	private String moddat;
+	private String modtim;
+}
