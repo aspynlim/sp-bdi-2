a2de7b8a0e61da375400728129036927983c890a
Jen Lim <cmdlhz@gmail.com>
2019-11-27 15:17:40 +0900

    2019-11-27 mybatis setup

--- src/main/java/com/sp/bdi2/user/UserDAOImpl.java
+++ src/main/java/com/sp/bdi2/user/UserDAOImpl.java
@@ -1,15 +1,10 @@
 package com.sp.bdi2.user;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -17,38 +12,19 @@ import org.springframework.stereotype.Repository;
 public class UserDAOImpl implements UserDAO {
 	
 	@Autowired
-	private BasicDataSource bds;
+	private SqlSessionFactory ssf;
 
 	@Override
 	public List<Map<String, String>> selectUserList() {
-		Connection con = null;
+		SqlSession ss = ssf.openSession();
 		
-		List<Map<String, String>> userList = new ArrayList<Map<String, String>>();
 		try {
-			con = bds.getConnection();
-			String sql = "select * from user_info";
-			PreparedStatement ps = con.prepareStatement(sql);
-			ResultSet rs = ps.executeQuery();
-			while(rs.next()) {
-				Map<String, String> user = new HashMap<String, String>();
-				user.put("uiNum", rs.getString("ui_num"));
-				user.put("uiName", rs.getString("ui_name"));
-				user.put("uiId", rs.getString("ui_id"));
-				user.put("credat", rs.getString("credat"));
-				user.put("cretim", rs.getString("cretim"));
-				userList.add(user);
-			}
-		} catch(SQLException e) {
+			return ss.selectList("com.sp.bdi2.dao.UserInfoMapper.selectUserInfoList");
+		} catch(Exception e) {
 			e.printStackTrace();
 		} finally {
-			if(con != null) {
-				try {
-					con.close();
-				} catch(SQLException e) {
-					e.printStackTrace();
-				}
-			}
+			ss.close();
 		}
-		return userList;
+		return null;
 	}
 }
